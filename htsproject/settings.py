d0a3ca176f88b883bcd821e0162589af5f9d30e7
Avinna Shrestha <44068341+abinstha@users.noreply.github.com>
2022-03-15 22:40:57 +0545

    Changing Debug mode

--- htsproject/settings.py
+++ htsproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@7lhbfmh9w$f*4m51%b6t4w6$&v!#ff1wor11=60w0xg8$&0bu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['167.99.241.99','www.himalayantibet.com','himalayantibet.com']
 
